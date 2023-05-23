33e075985263c54b264a8fe403ca5af1aeb4a6de
rakib-imtiaz <anonymous21120620@gmail.com>
2023-05-23 09:38:45 +0600

    added

--- app.py
+++ app.py
@@ -8,112 +8,59 @@ import pickle
 
 app = Flask(__name__)
 
-def predict_Parkinson(name, age, sex, test_time, motor_UPDRS, total_UPDRS, Jitter_percent, Jitter_Abs, Jitter_RAP, Jitter_PPQ5, Jitter_DDP, Shimmer, Shimmer_dB, Shimmer_APQ3, Shimmer_APQ5, Shimmer_APQ11, Shimmer_DDA, NHR, HNR, RPDE, DFA, PPE):
-    """Data Collection & Analysis"""
 
-    # loading the data from csv file to a Pandas DataFrame
+def train_parkinsons_model():
+    # Loading the data from the CSV file
     parkinsons_data = pd.read_csv('parkinsons.csv')
 
-    # number of rows and columns in the dataframe
-    num_rows, num_cols = parkinsons_data.shape
-
-    """Data Pre-Processing
-
-    Separating the features & Target
-    """
-
-    X = parkinsons_data.drop(columns=['name', 'status'], axis=1)
+    # Separating the features and target
+    X = parkinsons_data.drop(columns=['name','status'], axis=1)
     Y = parkinsons_data['status']
 
-    """Splitting the data to training data & Test data"""
-
+    # Splitting the data into training and test sets
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
 
-    """Model Training
-
-    Support Vector Machine Model
-    """
-
+    # Training the SVM model
     model = svm.SVC(kernel='linear')
-
-    # training the SVM model with training data
     model.fit(X_train, Y_train)
 
-    """Model Evaluation
-
-    Accuracy Score
-    """
-
-    # accuracy score on training data
-    X_train_prediction = model.predict(X_train)
-    training_data_accuracy = accuracy_score(Y_train, X_train_prediction)
-
-    # accuracy score on test data
-    X_test_prediction = model.predict(X_test)
-    test_data_accuracy = accuracy_score(Y_test, X_test_prediction)
-
-    """Building a Predictive System"""
-
-    input_data = [test_time, motor_UPDRS, total_UPDRS, Jitter_percent, Jitter_Abs, Jitter_RAP, Jitter_PPQ5, Jitter_DDP, Shimmer, Shimmer_dB, Shimmer_APQ3, Shimmer_APQ5, Shimmer_APQ11, Shimmer_DDA, NHR, HNR, RPDE, DFA, PPE]
-
-    # Convert input data to a numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-
-    # Reshape the numpy array
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-
-    # Make prediction using the loaded model
-    prediction = model.predict(input_data_reshaped)
-
-    if prediction[0] == 0:
-        result = "The person does not have Parkinson's disease."
-    else:
-        result = "The person has Parkinson's disease."
-
-    """Result Analysis"""
-
-    result_analysis = f"""*** Parkinson's Disease Prediction Analysis ***
-
-    Data Analysis:
-    - Number of rows in the dataset: {num_rows}
-    - Number of columns in the dataset: {num_cols}
-    - Training data accuracy: {training_data_accuracy}
-    - Test data accuracy: {test_data_accuracy}
-
-    Input Information:
-    - Name: {name}
-    - Age: {age}
-    - Sex: {sex}
-    - Test Time: {test_time}
-    - Motor UPDRS: {motor_UPDRS}
-    - Total UPDRS: {total_UPDRS}
-    - Jitter(%): {Jitter_percent}
-    - Jitter(Abs): {Jitter_Abs}
-    - Jitter(RAP): {Jitter_RAP}
-    - Jitter(PPQ5): {Jitter_PPQ5}
-    - Jitter(DDP): {Jitter_DDP}
-    - Shimmer: {Shimmer}
-    - Shimmer(dB): {Shimmer_dB}
-    - Shimmer(APQ3): {Shimmer_APQ3}
-    - Shimmer(APQ5): {Shimmer_APQ5}
-    - Shimmer(APQ11): {Shimmer_APQ11}
-    - Shimmer(DDA): {Shimmer_DDA}
-    - NHR: {NHR}
-    - HNR: {HNR}
-    - RPDE: {RPDE}
-    - DFA: {DFA}
-    - PPE: {PPE}
-
-    Prediction Result:
-    - {result}
-    """
-
-    """Saving the trained model"""
-
+    # Saving the trained model
     filename = 'parkinsons_model.sav'
     pickle.dump(model, open(filename, 'wb'))
 
-    return result_analysis
+def predict_parkinsons(name, features):
+    # Load the trained model
+    loaded_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+
+    # Create a DataFrame with the input features
+    input_data = pd.DataFrame([features], columns=[
+        'MDVP:Fo(Hz)', 'MDVP:Fhi(Hz)', 'MDVP:Flo(Hz)', 'MDVP:Jitter(%)',
+        'MDVP:Jitter(Abs)', 'MDVP:RAP', 'MDVP:PPQ', 'Jitter:DDP',
+        'MDVP:Shimmer', 'MDVP:Shimmer(dB)', 'Shimmer:APQ3', 'Shimmer:APQ5',
+        'MDVP:APQ', 'Shimmer:DDA', 'NHR', 'HNR', 'RPDE', 'DFA', 'spread1',
+        'spread2', 'D2', 'PPE'
+    ])
+
+    # Make the prediction
+    prediction = loaded_model.predict(input_data)
+
+    if prediction[0] == 0:
+        return f"{name} does not have Parkinson's disease."
+    else:
+        return f"{name} has Parkinson's disease."
+
+# Train the model (only needs to be done once)
+train_parkinsons_model()
+
+# Example usage:
+name = "John Doe"
+features = [197.07600, 206.89600, 192.05500, 0.00289, 0.00001, 0.00166, 0.00168, 0.00498,
+            0.01098, 0.09700, 0.00563, 0.00680, 0.00802, 0.01689, 0.00339, 26.77500,
+            0.422229, 0.741367, -7.348300, 0.177551, 1.743867, 0.085569]
+
+result = predict_parkinsons(name, features)
+print(result)
+
 
 
 @app.route('/')
@@ -129,19 +76,50 @@ def heart_disease():
     return render_template('heart_disease.html')
 
 @app.route('/parkinsons_disease', methods=['GET', 'POST'])
+
 def parkinsons_disease():
+
     if request.method == 'POST':
-        input1 = request.form.get('input1')
-        # Retrieve other form fields similarly
+        name = request.form.get('name')
+        age = request.form.get('age')
+        sex = request.form.get('sex')
         
-        # Print the form data in the console
-        print(f'Input 1: {input1}')
-        # Print other form fields similarly
+        input1 = float(request.form.get('MDVP_Fo_Hz'))
+        input2 = float(request.form.get('MDVP_Fhi_Hz'))
+        input3 = float(request.form.get('MDVP_Flo_Hz'))
+        input4 = float(request.form.get('Jitter_percent'))
+        input5 = float(request.form.get('Jitter_Abs'))
+        input6 = float(request.form.get('Jitter_RAP'))
+        input7 = float(request.form.get('ppq'))
+        input8 = float(request.form.get('Jitter_DDP'))
+        input9 = float(request.form.get('Shimmer'))
+        input10 = float(request.form.get('Shimmer_dB'))
+        input11 = float(request.form.get('Shimmer_APQ3'))
+        input12 = float(request.form.get('Shimmer_APQ5'))
+        input13 = float(request.form.get('MDVP_APQ'))
+        input14 = float(request.form.get('Shimmer_DDA'))
+        input15 = float(request.form.get('NHR'))
+        input16 = float(request.form.get('HNR'))
+        input17 = float(request.form.get('RPDE'))
+        input18 = float(request.form.get('DFA'))
+        input19 = float(request.form.get('Spread1'))
+        input20 = float(request.form.get('Spread2'))
+        input21 = float(request.form.get('D2'))
+        input22 = float(request.form.get('PPE'))
+
+        # Example usage:
+        features = [
+            input1, input2, input3, input4, input5, input6, input7, input8, input9, input10,
+            input11, input12, input13, input14, input15, input16, input17, input18, input19,
+            input20, input21, input22
+        ]
+        result = predict_parkinsons(name, features)
         
-        # Perform your prediction logic here
+
+        print(result)
         
-        return 'Prediction result'
-    
+        return result
+        
     return render_template('parkinsons_disease.html')
 
 if __name__ == '__main__':
